d33005f9d94c2f07dbd3b166247a6db76ab8a982
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2022-08-04 09:20:12 +0100

    header and routes set up

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,70 @@
+<template>
+  <header>
+    <nav>
+      <h1><router-link to="/">Hire a Developer</router-link></h1>
+      <ul>
+        <li><router-link to="/developers">All Developers</router-link></li>
+        <li><router-link to="/offers">Offers</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #004ba3;
+  width: 100%;
+  height: 8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+header a {
+  text-decoration: none;
+  color: #fff;
+  padding: 0.5rem;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  background: #001b39;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  background: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import DevelopersDetails from './pages/developers/DevelopersDetails.vue';
+import DevelopersList from './pages/developers/DevelopersList.vue';
+import DeveloperRegistration from './pages/developers/DevelopersRegistration';
+import ReceivedOffers from './pages/offers/ReceivedOffers.vue';
+import SendOffer from './pages/offers/SendOffer.vue';
+import NotFound from './pages/NotFound.vue'
+
 const router = createRouter({
-  history: createWebHistory,
+  history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/developers' },
-    { path: '/developers', component: null },
+    { path: '/developers', component: DevelopersDetails },
     {
-      path: 'developers/:id',
-      component: null,
-      children: [{ path: 'send-offer', component: null }],
+      path: '/developers/:id',
+      component: DevelopersList,
+      children: [{ path: 'send-offer', component: SendOffer }],
     },
-    { path: '/register', component: null },
-    { path: '/offers', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: DeveloperRegistration },
+    { path: '/offers', component: ReceivedOffers },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
